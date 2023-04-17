46fdd8933d7bbb4c097420458246e6c4bdddce05
Dasha Burgos <dasha.lewis@gmail.com>
2023-04-17 19:14:03 -0400

    created file and added proof of life and function

--- basics/Main.java
+++ basics/Main.java
@@ -1,5 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-    
+    System.out.println("Proof of Life");
+  }
+
+  public static String pluralize(String word, int count){
+    if (count == 0 || count > 1){
+      return word + "s";
+    } else {
+      return word;
+    }
   }
 }
