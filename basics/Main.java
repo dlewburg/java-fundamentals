fc1ffc311e4934eb0e67a8b771678a68ffbcd33a
Dasha Burgos <dasha.lewis@gmail.com>
2023-04-17 19:45:33 -0400

    completed pluralize feature

--- basics/Main.java
+++ basics/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Proof of Life");
+    pluralize("fish", 0);
+    System.out.println(pluralize( "fish", 0));
   }
 
   public static String pluralize(String word, int count){
@@ -10,4 +11,6 @@ public class Main {
       return word;
     }
   }
+
+  
 }
